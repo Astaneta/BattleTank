f98cd87dca268f56266a558b340c646046d63e54
Federico Pelliccia <pellicciafederico@gmail.com>
2017-09-08 11:10:08 +0200

    Added new c++ pawn class, the Tank.h, and a new APlayerController, The TankPlayerController. Changed the inheritance of the BP with the new classes

---
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Tank.h"
+#include "TankPlayerController.generated.h"
+
+
+
+
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	ATank* GetControlledTank() const;
+	
+	
+};
